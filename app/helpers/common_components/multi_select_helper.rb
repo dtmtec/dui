155a41acb4fad3b4f95754df8b91274c1e619a4c
Vicente Mundim <vicente.mundim@gmail.com>
2012-10-30 15:10:26 -0200

    Adding a view and helpers for using multi select component

---
+++ app/helpers/common_components/multi_select_helper.rb
@@ -0,0 +1,27 @@
+module CommonComponents
+  module MultiSelectHelper
+    def render_multi_select(options)
+      render 'common_components/multi_select/multi_select', {
+        multi_select_available_url: options[:available_url],
+        multi_select_search_placeholder: i18n_for('search_placeholder', options[:scope]),
+        multi_select_all_label: i18n_for('all', options[:scope]),
+        multi_select_available_title: i18n_for('available.title', options[:scope]),
+        multi_select_selected_title: i18n_for('selected.title', options[:scope]),
+        multi_select_no_available_items_message: i18n_for('available.no_items_message', options[:scope]),
+        multi_select_no_selected_items_message: i18n_for('selected.no_items_message', options[:scope])
+      }
+    end
+
+    private
+      def i18n_key_for(key, scope=nil)
+        scope = scope ? ".#{scope}" : ''
+        "multi_select#{scope}.#{key}"
+      end
+
+      def i18n_for(key, scope=nil)
+        options = { default: I18n.t(i18n_key_for(key)) }
+
+        I18n.t(i18n_key_for(key, scope), options)
+      end
+  end
+end
