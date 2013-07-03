1642e600d971b0bc27a526d5b407afe0ada60b0e
Vicente Mundim <vicente.mundim@gmail.com>
2013-07-03 12:17:13 -0300

    Not mapping route /dui/upload when running on production

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Dui::Engine.routes.draw do
-  post 'upload' => 'uploader#upload', as: :uploader
+  post 'upload' => 'uploader#upload', as: :uploader unless Rails.env.production?
   get 'status' => 'uploader#status', as: :uploader_status
   get 'result' => 'uploader#result', as: :uploader_iframe_redirect
 end
